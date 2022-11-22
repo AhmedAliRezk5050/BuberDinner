0f073aef8483d13d767d4511407d8b7c002ffb02
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-22 13:02:38 +0200

    Edits

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -4,7 +4,6 @@ using BuberDinner.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 {
-    var x = builder.Configuration;
   builder.Services.AddApplication();
   builder.Services.AddInfrastructure(builder.Configuration);
   builder.Services.AddControllers();
