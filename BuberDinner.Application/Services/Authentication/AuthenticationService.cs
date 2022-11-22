36e5e5532ed6baf9cedf0e5e9eeead8d2c039a54
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-22 20:27:27 +0200

    Repository implement and use IUserRepositiry

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -27,9 +27,9 @@ public class AuthenticationService : IAuthenticationService
             throw new Exception("Invalid password");
         }
 
-        var token = _jwtTokenGenerator.GenerateToken(user.Id, firstName: user.FirstName, lastName: user.LastName);
+        var token = _jwtTokenGenerator.GenerateToken(user);
 
-        return new AuthenticationResult(user.Id, user.FirstName, user.LastName, email, token);
+        return new AuthenticationResult(user, token);
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
@@ -49,8 +49,8 @@ public class AuthenticationService : IAuthenticationService
 
         _userRepository.Add(user);
 
-        var token = _jwtTokenGenerator.GenerateToken(user.Id, firstName, lastName);
+        var token = _jwtTokenGenerator.GenerateToken(user);
 
-        return new AuthenticationResult(user.Id, firstName, lastName, email, token);
+        return new AuthenticationResult(user, token);
     }
 }
