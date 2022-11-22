79166a74ae2135c585f23d4ec4a99f0be6ba10a8
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-22 22:28:08 +0200

    Error handling using Middleware

---
+++ BuberDinner.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+
+namespace BuberDinner.Api.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(new ErrorDetails(
+            context.Response.StatusCode,
+            exception.Message
+            ).ToString());
+        }
+    }
+
+    internal record ErrorDetails(int StatusCode, string Message);
+}
