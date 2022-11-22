c16cd9a5775d1a078167ff5ce9bc8328c22fb063
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-22 08:05:47 +0200

    Generate token interface

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,16 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+  private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+  public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+  {
+    _jwtTokenGenerator = jwtTokenGenerator;
+  }
+
   public AuthenticationResult Login(string email, string password)
   {
     return new AuthenticationResult(Guid.NewGuid(), "ahmed", "ali", email, "token");
@@ -9,6 +18,11 @@ public class AuthenticationService : IAuthenticationService
 
   public AuthenticationResult Register(string firstName, string lastName, string email, string password)
   {
-    return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+
+    Guid id = Guid.NewGuid();
+
+    var token = _jwtTokenGenerator.GenerateToken(id, firstName, lastName);
+
+    return new AuthenticationResult(id, firstName, lastName, email, token);
   }
 }
