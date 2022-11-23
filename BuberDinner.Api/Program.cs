c22a7a336f7ff1f10ccbb56b1e41e937e59d3dfb
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-23 10:44:40 +0200

    Error handling using error endpoint add custom Problem Details Factory to provide more values to the error response

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,23 +1,23 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filters;
 using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 {
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers(
-        options => options.Filters.Add<ErrorHandlingFilterAttribute>()
-        );
+    builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
-
     app.MapControllers();
-
     app.Run();
 }
