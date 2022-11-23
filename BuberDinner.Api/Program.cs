acc0de7a9a8500b696eb5d525c9edb6e73e7c7be
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-23 12:32:11 +0200

    Edits

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,4 @@
 using BuberDinner.Api.Errors;
-using BuberDinner.Api.Filters;
-using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
