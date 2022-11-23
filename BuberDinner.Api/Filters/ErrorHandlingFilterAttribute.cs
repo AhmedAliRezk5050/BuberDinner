5e9e05a7a7ac6980963d71928b6da8a7c2cb4481
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-23 08:08:36 +0200

    Error handling using filters

---
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BuberDinner.Api.Filters
+{
+    public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+            context.Result = new ObjectResult(null)
+            {
+                StatusCode = 500,
+                Value = exception.Message
+            };
+            context.ExceptionHandled = true;
+        }
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,3 +1,4 @@
+using BuberDinner.Api.Filters;
 using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
@@ -5,17 +6,18 @@ using BuberDinner.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 {
-  builder.Services.AddApplication();
-  builder.Services.AddInfrastructure(builder.Configuration);
-  builder.Services.AddControllers();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddControllers(
+        options => options.Filters.Add<ErrorHandlingFilterAttribute>()
+        );
 }
 
 var app = builder.Build();
 {
-    app.UseMiddleware<ErrorHandlingMiddleware>();   
-  app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
-  app.MapControllers();
+    app.MapControllers();
 
-  app.Run();
+    app.Run();
 }
